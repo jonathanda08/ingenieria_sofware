2ed10777294dc8666c8cf8fbbed87266df47cdd8
jonathan <jduartes1@ucentral.edu.co>
2024-04-18 18:27:16 -0500

    master: cambio de archivos

---
+++ src/main/java/co/ucentrel/gestionador/de/notas/GestionadorDeNotasApplication.java
@@ -0,0 +1,13 @@
+package co.ucentrel.gestionador.de.notas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionadorDeNotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionadorDeNotasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/ucentrel/gestionador/de/notas/GestionadorDeNotasApplicationTests.java
@@ -0,0 +1,13 @@
+package co.ucentrel.gestionador.de.notas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionadorDeNotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
